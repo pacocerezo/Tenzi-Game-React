a83709dd70b210c30bc9221983bbe76732c31474
pacocerezo <pacoceval@gmail.com>
2023-02-16 13:00:16 +0100

    Added unfinished game

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white",
+    };
+    return (
+        <div className="die" style={styles} onClick={props.holdDice}>
+            <h2>{props.value}</h2>
+        </div>
+    );
+}
